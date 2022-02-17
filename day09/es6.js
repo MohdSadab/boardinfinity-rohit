7af171726732636f120a2019b1db741645dd750b
MohdSadab <sadabkhan14198@gmail.com>
2022-02-17 21:36:07 +0530

    added the es6 whole concept with this keyword

--- day09/es6.js
+++ day09/es6.js
@@ -165,27 +165,182 @@
  * rejected
  */
 
-const promise1 = new Promise((resolve,reject)=>{
-    console.log("started")
-    setTimeout(()=>{
-       console.log("i am first")
-       resolve('i am resolved') 
-    },1000)
-})
 
 
-const promise2 = new Promise((resolve,reject)=>{
-    setTimeout(()=>{
-       console.log("i am second") 
-       resolve()
-    },100)
-})
+// function promise1(){
+//    return new Promise((resolve,reject)=>{
+//     console.log("started")
+//     setTimeout(()=>{
+//        console.log("i am first")
+//        resolve('i am resolved') 
+//     // reject()
+//     },1000)
+// }) 
+// }
 
-promise1.then(data=>{
-   console.log("dat",data)
-   return data
-}).then(data=>{
-    console.log("ended")
-}).catch(error=>{
-    console.log(error,">>>>>>>>>>>")
-})
+// function promise2(){
+//     return new Promise((resolve,reject)=>{
+//      console.log("started")
+//      setTimeout(()=>{
+//         console.log("i am second")
+//         resolve('i am resolved') 
+//      },100)
+//  }) 
+//  }
+
+
+// // promise1().then(data=>{
+// //     console.log("then called >>>")
+// //     return promise2();
+// // }).then(data=>{
+// //     console.log("second then")
+// // }).catch(error=>{
+// //     console.log("catch called")
+// // })
+
+// //1-> promise - ---- resolved --- then --- return promise repeat  1 and 2
+// //2->  promise - ---- reject --- catch --- return promise repeat  1 and 2
+
+// // from avoiding above issue async await came
+
+// // async function printSynchronously(){
+
+// //     await promise1();
+// //     await promise2();
+// // }
+
+// // printSynchronously();
+// // console.log(" i am last")
+
+
+
+// // this is refer to the thing which cause to call
+
+// var name="outer";
+// var city= " outer city"
+
+// // here this refer to the object which is calling the getIntro method
+// // let obj={
+// //     name:"sadab",
+// //     city:"Kanpur",
+// //     getIntro:function(){
+// //         return this.name+" "+this.city  
+// //     }
+// // }
+
+// // here this refer to the context by default in unstrict manner this refer to it's global object which is window object
+// // let obj={
+// //     name:"sadab",
+// //     city:"Kanpur",
+// //     getIntro:()=>{
+// //         return this.name+" "+this.city
+// //     }
+// // }
+
+
+// // let obj2={
+// //     name:"sadabsshs",
+// //     city:"Kanpurssjjsj",
+// //     getIntro: obj.getIntro()
+// // }
+// // console.log(obj2);
+// // let val= obj.getIntro().split(' ');
+// // if(val==="sadab"){
+// //     console.log("Welcome ")
+// // }else{
+// //     console.log("not authoried")
+// // }
+
+
+
+
+// class Polygon{
+
+//     // static property of class can be called without creating instance
+//     static displayName = "Point";
+//     constructor(width,height){
+//         this.width=width;
+//         this.height=height
+//     }
+
+//     //  can not be called without creating instance
+//     parent(){
+//         console.log("i am inherited",this.getArea())
+//     }
+
+// }
+
+// class Square extends Polygon{
+
+//     constructor(width,height){
+//         // calling parent constructor
+//         super(width,height)
+//     }
+
+//     getArea(){
+//         return this.width*this.height
+//     }
+// }
+
+// class Rectangle extends Polygon{
+
+//     constructor(width,height){
+//         // calling parent constructor
+//         super(width,height)
+//     }
+
+//     getArea(){
+//         return this.width*this.height
+//     }
+// }
+
+// // creating instance of square
+// const squ = new Square(10,10);
+// console.log(squ.getArea());
+
+// const rect = new Rectangle(10,20);
+// console.log(rect.getArea());
+
+// squ.parent();
+// rect.parent();
+
+// console.log(Polygon.displayName)
+
+// //  college management
+
+// //  class person => student , teacher , principle, staff  ( teach => which subject) (student => wich stream)
+
+// //  ajax call
+// function getData(){
+//     fetch('https://jsonplaceholder.typicode.com/todos/1').then(response => response.json()).then(json => console.log(json)) 
+// }
+
+// getData()
+
+
+// in normal function we can use call apply and bind functionality
+// but in arrow function we can't
+
+let obj1={
+    name:"sadab",
+    city:"Kanpur",
+    getIntro:function(age){
+        return this.name+" "+this.city+" age is "+ age
+    }
+}
+
+let obj2 = {
+    name:"Rahul",
+    "city":"Delhi"
+}
+
+// call using we can change this refrence
+//  function.call(thisArg, functionArgument)
+console.log(obj1.getIntro.call(obj2,25));
+
+// apply is same as call but diffrence is we passed the array of function Argument
+console.log(obj1.getIntro.call(obj2,[25,35]));
+
+// bind same as call but returns a function which we can call later
+let myNewFunc = obj1.getIntro.bind(obj2,26);
+console.log(myNewFunc())
